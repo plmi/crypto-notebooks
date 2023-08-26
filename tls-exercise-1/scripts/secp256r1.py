c76693167ad560f1b6c16b47e607f9248d6ceef1
plmi <65455617+plmi@users.noreply.github.com>
2023-08-26 17:19:28 +0200

    Fix formatting

--- tls-exercise-1/scripts/secp256r1.py
+++ tls-exercise-1/scripts/secp256r1.py
@@ -4,12 +4,12 @@ from collections import namedtuple
 Point = namedtuple("Point", "x y")
 
 if len(sys.argv) != 4:
-	print("Usage: secp256r1.py [PubX] [PubY] [secret]")
+    print("Usage: secp256r1.py [PubX] [PubY] [secret]")
 else:
-	x = int(sys.argv[1], 0)
-	y = int(sys.argv[2], 0)
-	scalar = int(sys.argv[3], 0)
-	
-	publicKey = Point(x, y)
-	sharedSecret = eccFuncs.computeShared(publicKey, scalar)
-	print("Computed Point: (" + str(sharedSecret.x) + "," + str(sharedSecret.y) + ")")
+    x = int(sys.argv[1], 0)
+    y = int(sys.argv[2], 0)
+    scalar = int(sys.argv[3], 0)
+
+    publicKey = Point(x, y)
+    sharedSecret = eccFuncs.computeShared(publicKey, scalar)
+    print("Computed Point: (" + str(sharedSecret.x) + "," + str(sharedSecret.y) + ")")
